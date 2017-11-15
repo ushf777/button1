484e0d13eff4a11e7133e00e686f8b97784789e3
jintae <606jintae@naver.com>
2017-11-15 11:43:14 +0900

    birthday

--- button1.py
+++ button1.py
@@ -4,7 +4,8 @@ window =Tk()
 
 b1 = Button(window, text="kim jintae")
 b2 = Button(window, text="60142263")
+b3 = Button(window, text="19940314")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
-
+b3.grid(row=1, column=0)
 window.mainloop()
